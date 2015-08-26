a965d57ceaed723577d7ab8793a8397eb8e5170b
gary <gwleong@gmail.com>
2015-08-26 22:37:56 +0000

    added first commit

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'docker-mongodb'
+maintainer       'Gary Leong'
+maintainer_email 'gwleong@gmail.com'
+license          'MIT License'
+description      'to mongodb in container'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.1'
+
+depends "docker"
